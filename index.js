cf2f4883efcf24664d42a7b1f25ed43ed4b6dc06
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-12-07 15:00:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+const submitData = {
+    userName: "jensam",
+    userEmail: "jensam@gmail.com",
+  };  
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+body: JSON.stringify(submitData)
+};
+fetch("http://localhost:3000/users ", configurationObject)
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
